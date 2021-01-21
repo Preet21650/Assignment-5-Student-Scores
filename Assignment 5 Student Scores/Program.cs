c8d33277f49e42d126747a8aa036d5f6a4315087
Preetmohan Manchanda <preetmohansm@outlook.com>
2021-01-21 14:50:45 +0700

    Add project files.

---
+++ Assignment 5 Student Scores/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assignment_5_Student_Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] preetScores = { 7, 6, 5, 4, 3 };
+            Student Preet = new Student("Preet", 12, preetScores);
+
+            Console.WriteLine(Preet.GetHighest());
+        }
+    }
+}
---
+++ Assignment 5 Student Scores/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_5_Student_Scores
+{
+    class Student
+    {
+        private string name;
+        private int year;
+        // scores -----------> object of 5 spaces
+        private int[] scores = new int[5];
+
+        public Student(string name, int year,int[] scores)
+        {
+            this.name = name;
+            this.year = year;
+            //scores ----------> other object containing a number of elements
+            this.scores = scores;
+        }
+        public double GetAverage()
+        {
+            int total = 0;
+            for(int i = 0; i < scores.Length; i++)
+            {
+                total = total + scores[i];
+            }
+            double average =(double) total / scores.Length;
+            return average;
+        }
+        public int GetHighest()
+        {
+            int highest = scores[0];
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] > highest) highest = scores[i];
+            }
+                return highest;
+            
+        }
+        public int GetLowest()
+        {
+            int lowest = scores[0];
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] < lowest) lowest = scores[i];
+            }
+            return lowest;
+
+        }
+        public int[] GetScores()
+        {
+            return scores;
+        }
+    }
+}
